41895791f610b4f2b03f3e884845cf7442bdccbc
hustlers-tech <hustlerstech.com@gmail.com>
2025-09-07 22:24:57 +0530

    initial commit - test

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col w-full h-screen justify-center items-center text-4xl">
+      <p>Welcome to hustlerstech.com</p>
+      <p>
+        [ This is Test Deployment ]
+      </p>
+      <p className="text-green-500">LAUNCHING SOON</p>
+    </div>
+  );
+}
