235536ea947b6cb338e43db1a2b0985b79a9da51
hustlers-tech <hustlerstech.com@gmail.com>
2025-09-12 22:52:50 +0530

    Minor Updates

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <div className="flex flex-col w-full h-screen justify-center items-center text-4xl">
       <p>Welcome to hustlerstech.com</p>
-      <p>
-        [ This is Test Deployment ]
-      </p>
       <p className="text-green-500">LAUNCHING SOON</p>
     </div>
   );
