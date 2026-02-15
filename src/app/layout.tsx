6c245c52e1b93c6eaa11b5495ccf445a6e7d70fb
hustlers-tech <hustlerstech.com@gmail.com>
2026-02-15 17:55:39 +0530

    updated dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${montserrat.variable} antialiased`}>
-        <Navbar/>
         {children}
       </body>
     </html>
